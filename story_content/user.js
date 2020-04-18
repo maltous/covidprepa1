b758964f9193c56ac56779260a176416cfb87af0
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-04-18 22:56:51 +0300

    covidprepa1

---
+++ story_content/user.js
@@ -0,0 +1,39 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "67dWUXfMCUW":
+        Script1();
+        break;
+      case "5xQa5riWcoR":
+        Script2();
+        break;
+      case "5yZgfhNKqSF":
+        Script3();
+        break;
+      case "6b7aSt90KSt":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script4()
+{
+  window.print();
+}
+
